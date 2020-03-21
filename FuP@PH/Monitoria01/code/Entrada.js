865f5400c43bcf1e6dde0ee6e41b74d08c0d95ee
HenrickyL <henrickyL1@gmail.com>
2020-03-20 23:04:39 -0300

    up

--- FuP@PH/Monitoria01/code/Entrada.js
+++ FuP@PH/Monitoria01/code/Entrada.js
@@ -1,12 +1,14 @@
 //codigo que sempre tem que usa
+	//basta copiar essas 3 linha antes do seu código
 var fs = require("fs"); //biblioteca padrao do NODE
 let __lines = fs.readFileSync(0).toString().split("\n");
 let getline = () => __lines.shift(); //so copiar essas 3 linhas
+//Referência: https://youtu.be/fXi9SQ84ibM (ignore o audio)
 //termina aqui
 
-//Seu codigo
+//Escreva seu Código apartir daqui--------------------------------
 
-var a = Number(getline())
+var a = Number(getline())//comando de entrada e conversão para Número
 var b = Number(getline())
 //Crtl+D
 if(a > b){
